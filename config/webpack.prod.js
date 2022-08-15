a4d5ea7a14cd64e1845b144d68185724ad225498
Padelpop Eskrim <fadhel@femaledaily.com>
2022-08-15 14:08:59 +0700

    feat:update

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -9,7 +9,7 @@ const prodConfig = {
     mode: 'production',
     output: {
         filename: '[name].[contenthash].js',
-        publicPath: '/public'
+        publicPath: '/'
     },
     plugins: [
         new ModuleFederationPlugin({
